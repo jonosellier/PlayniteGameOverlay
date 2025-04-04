17e9cfdb5328116ca2bd750c996ec97ae4a132b0
Jonathan Sellier <jonosellier@gmail.com>
2025-04-04 17:01:46 -0400

    Beginnings of button bar

---
+++ ButtonItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PlayniteGameOverlay
+{
+    public enum ButtonAction
+    {
+        Directory,
+        KeyboardShortcut,
+        Uri,
+        Executable,
+        Shortcut
+    }
+
+    public class ButtonItem
+    {
+        public string IconPath { get; set; }
+        public string Title { get; set; }
+        public ButtonAction ActionType { get; set; }
+        public string Path { get; set; }
+    }
+}
